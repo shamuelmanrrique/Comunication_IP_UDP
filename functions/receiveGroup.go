fc5f6700c9129f146054098ad9f9aa8b1a6a620a
shamuel21 <shamuelmanrrique@gmail.com>
2019-10-21 08:28:52 +0200

    fix(): problems msm send when you init process

--- functions/receiveGroup.go
+++ functions/receiveGroup.go
@@ -2,11 +2,13 @@ package functions
 
 import (
 	"fmt"
+	v "practice1/vclock"
+	"sort"
 )
 
 // ReceiveGroup snks
 func ReceiveGroup(connect Connection, n int) error {
-	// var sortMsms []Message
+	var arrayMsms []Message
 	bufferMsm := make(chan Message, len(connect.GetIds()))
 	var err error
 	vector := connect.GetVector()
@@ -21,6 +23,16 @@ func ReceiveGroup(connect Connection, n int) error {
 		//TODO  -> Organizar elementos que me llegaron
 		vector.Merge(msm.GetVector())
 
+		// Guardo el msm en un array de msm 
+		arrayMsms = append(arrayMsms, msm)
+
+		// Ordeno el arreglo de msm
+		sort.SliceStable(arrayMsms, func(i, j int) bool {
+			return arrayMsms[i].Vector.Compare(arrayMsms[j].Vector, v.Descendant)
+		})
+
+		// Meto el vector act
+		connect.GetVector().Merge(vector)
 		if msm.GetFrom() == id {
 			go SendGroup(connect)
 		}
@@ -28,7 +40,6 @@ func ReceiveGroup(connect Connection, n int) error {
 		if ok == false {
 			break
 		}
-
 	}
 
 	return err
