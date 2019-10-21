d3520a1d271c1d63fc8da7c34baf84852dabf0ab
shamuel21 <shamuelmanrrique@gmail.com>
2019-10-21 11:49:39 +0200

    fix(flag): created flags.go

--- app/main.go
+++ app/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"strings"
 
 	// "net"
 
@@ -20,50 +18,17 @@ const (
 	port = ":5001"
 )
 
-// Example 3: A user-defined flag type, a slice of durations.
-type interval []time.Duration
+// var DelaysFlag Delays
 
-// String is the method to format the flag's value, part of the flag.Value interface.
-// The String method's output will be used in diagnostics.
-func (i *interval) String() string {
-	return fmt.Sprint(*i)
-}
-
-// Set is the method to set the flag value, part of the flag.Value interface.
-// Set's argument is a string to be parsed to set the flag.
-// It's a comma-separated list, so we split it.
-func (i *interval) Set(value string) error {
-	// If we wanted to allow the flag to be set multiple times,
-	// accumulating values, we would delete this if statement.
-	// That would permit usages such as
-	//	-deltaT 10s -deltaT 15s
-	// and other combinations.
-	if len(*i) > 0 {
-		return errors.New("interval flag already set")
-	}
-	for _, dt := range strings.Split(value, ",") {
-		duration, err := time.ParseDuration(dt)
-		if err != nil {
-			return err
-		}
-		*i = append(*i, duration)
-	}
-	return nil
-}
-
-// Define a flag to accumulate durations. Because it has a special type,
-// we need to use the Var function and therefore create the flag during
-// init.
-
-var intervalFlag interval
-
-func init() {
-	// Tie the command-line flag to the intervalFlag variable and
-	// set a usage message.
-	flag.Var(&intervalFlag, "deltaT", "comma-separated list of intervals to use between events")
-}
-
+// func init() {
+// 	// Tie the command-line flag to the DelaysFlag variable and
+// 	// set a usage message.
+// 	flag.Var(&DelaysFlag, "deltaT", "comma-separated list of Delayss to use between events")
+// }
 func main() {
+	var DelaysFlag f.Delays
+	flag.Var(&DelaysFlag, "deltaT", "comma-separated list of Delayss to use between events")
+
 	// var intervalFlag interval
 	//Estoy coloacando cuatro procesos por defoult
 	// var process = flag.Int("process", 4, "numero de procesos que quieres crear")
@@ -76,7 +41,7 @@ func main() {
 
 	//var delay = flag.Duration("delay",, "arreglo de retardos")
 	//var procesos = flag.Int("procesos", 4, "numero de procesos que quieres crear")
-	// fmt.Println(.String())
+	fmt.Println(DelaysFlag[1:])
 	// fmt.Println("count value ", *process)
 	fmt.Printf("###################### MAIN  %s ########################### \n", ip+port)
 	d := []int{5, 8}
